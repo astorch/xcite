82a53293bed079e4f602975af1b6ebdca1171bba
Arian Storch <arian.storch@evy-solutions.de>
2017-08-24 13:41:29 +0200

    Added additional unit test

--- source/tests/xcite.collections.tests/LinearListSortingTests.cs
+++ source/tests/xcite.collections.tests/LinearListSortingTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using NUnit.Framework;
+using NUnit.Framework.Constraints;
 
 namespace xcite.collections.tests {
     [TestFixture]
@@ -14,6 +15,30 @@ namespace xcite.collections.tests {
 
             int[] sortedList = list.ToArray();
             CollectionAssert.AreEqual(new[] {1, 2, 3, 4, 5, 6, 7}, sortedList);
+        }
+
+        [Test]
+        public void SortWithZero() {
+            LinearList<int> list = new LinearList<int>(new[]{2,0,1});
+            list.Sort((i1, i2) => i1.CompareTo(i2));
+
+            int[] sortedList = list.ToArray();
+            CollectionAssert.AreEqual(new[] {0, 1, 2}, sortedList);
+        }
+
+        [Test]
+        public void SortWithProperty() {
+            IndexObject[] objetSet = new IndexObject[] {
+                new IndexObject {Id = "eins", Index = 2},
+                new IndexObject {Id = "zwei", Index = 0},
+                new IndexObject {Id = "drei", Index = 1}
+            };
+
+            LinearList<IndexObject> list = new LinearList<IndexObject>(objetSet);
+            list.Sort(item => item.Index);
+
+            IndexObject[] sortedSet = list.ToArray();
+            CollectionAssert.AreEqual(new IndexObject[] {objetSet[1], objetSet[2], objetSet[0]}, sortedSet);
         }
 
         [Test]
@@ -52,6 +77,12 @@ namespace xcite.collections.tests {
                 dataSet[i] = r.Next(max) + 1;
             }
             return dataSet;
+        }
+
+        class IndexObject {
+            public string Id { get; set; }
+
+            public int Index { get; set; }
         }
     }
 }
