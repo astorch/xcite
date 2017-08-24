88e14a250976b28eb37a011b76b56e09fd70ce97
Arian Storch <arian.storch@evy-solutions.de>
2017-08-24 12:11:31 +0200

    Introducing ISortable

---
+++ source/xcite.collections/ISortable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+namespace xcite.collections {
+    /// <summary> Announces that this type supports sorting of its elements. </summary>
+    public interface ISortable {
+        /// <summary>
+        /// Sorts using the given <paramref name="comparison"/> in ascending order.
+        /// </summary>
+        /// <param name="comparison">Comparison</param>
+        void Sort(Comparison<object> comparison);
+
+        /// <summary>
+        /// Sorts using the given <paramref name="comparer"/> in ascending order.
+        /// </summary>
+        /// <param name="comparer">Comparer</param>
+        void Sort(IComparer comparer);
+
+        /// <summary>
+        /// Sorts using the property referenced by the given <paramref name="propertyReference"/> 
+        /// in ascending order.
+        /// </summary>
+        /// <typeparam name="TProperty">Type of property</typeparam>
+        /// <param name="propertyReference">Property reference</param>
+        void Sort<TProperty>(Func<object, TProperty> propertyReference);
+    }
+}
---
+++ source/xcite.collections/ISortable`1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace xcite.collections {
+    /// <summary> Announces that this generic type supports sorting of its elements. </summary>
+    /// <typeparam name="TElement">Type of managed elements</typeparam>
+    public interface ISortable<TElement> : ISortable {
+
+        /// <summary>
+        /// Sorts using the given <paramref name="comparison"/>.
+        /// </summary>
+        /// <param name="comparison">Comparison</param>
+        void Sort(Comparison<TElement> comparison);
+
+        /// <summary>
+        /// Sorts using the given <paramref name="comparer"/>.
+        /// </summary>
+        /// <param name="comparer">Comparer</param>
+        void Sort(Comparer<TElement> comparer);
+
+        /// <summary>
+        /// Sorts using the property referenced by the given <paramref name="propertyReference"/>.
+        /// </summary>
+        /// <typeparam name="TProperty">Type of property</typeparam>
+        /// <param name="propertyReference">Property reference</param>
+        void Sort<TProperty>(Func<TElement, TProperty> propertyReference);
+    }
+}
