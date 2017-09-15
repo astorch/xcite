70cf0fd47f4650916fcd2e109d1348e81d928d40
Arian Storch <arian.storch@evy-solutions.de>
2017-09-15 11:30:06 +0200

    Introducing IObservableObject

---
+++ source/xcite.csharp/AbstractObservableObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace xcite.csharp {
+    /// <summary> Abstract implementation of <see cref="IObservableObject"/>. </summary>
+    public abstract class AbstractObservableObject : IObservableObject {
+        /// <inheritdoc />
+        public event ObjectChangedHandler ObjectChanged;
+
+        /// <summary>
+        /// Raises the <see cref="ObjectChanged"/> event for the property or method that invokes 
+        /// this method.
+        /// </summary>
+        /// <param name="propertyName">Name of the changed property</param>
+        protected virtual void OnObjectChanged([CallerMemberName] string propertyName = null) {
+            RaiseObjectChanged(propertyName);
+        }
+
+        /// <summary>
+        /// Raises the <see cref="ObjectChanged"/> event for the specified <paramref name="propertyName"/>.
+        /// </summary>
+        /// <param name="propertyName">Name of the changed property</param>
+        protected virtual void RaiseObjectChanged(string propertyName) {
+            ObjectChanged?.Invoke(this, propertyName);
+        }
+    }
+}
---
+++ source/xcite.csharp/IObservableObject.cs
@@ -0,0 +1,14 @@
+﻿namespace xcite.csharp {
+    /// <summary>
+    /// Signature of a handler for the <see cref="IObservableObject.ObjectChanged"/> event.
+    /// </summary>
+    /// <param name="sender">Sender</param>
+    /// <param name="propertyName">Name of the changed property</param>
+    public delegate void ObjectChangedHandler(object sender, string propertyName);
+
+    /// <summary> Object that raises an event when an owned property has been changed. </summary>
+    public interface IObservableObject {
+        /// <summary> Is raises when an owned property has been changed. </summary>
+        event ObjectChangedHandler ObjectChanged;
+    }
+}
