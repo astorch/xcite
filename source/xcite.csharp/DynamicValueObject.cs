ad6f38fdb8139e31fd387677901cec7063391d09
Arian Storch <arian.storch@evy-solutions.de>
2017-10-19 20:17:45 +0200

    Introducing IDynamicValueObject

---
+++ source/xcite.csharp/DynamicValueObject.cs
@@ -0,0 +1,36 @@
+﻿namespace xcite.csharp {
+    /// <summary> Provides an implementation of <see cref="IDynamicValueObject{TValue}"/>. </summary>
+    /// <typeparam name="TValue">Type of value being wrapped</typeparam>
+    public class DynamicValueObject<TValue> : IDynamicValueObject<TValue> {
+        private TValue _value;
+
+        /// <summary>
+        /// Initialies the instance with the specified <paramref name="value"/>.
+        /// </summary>
+        /// <param name="value">Initial value</param>
+        public DynamicValueObject(TValue value) {
+            _value = value;
+        }
+
+        /// <summary> Initializes the instance with the value type-specified default value. </summary>
+        public DynamicValueObject() : this(default(TValue)) {
+            // Nothing to do here
+        }
+
+        /// <inheritdoc />
+        public event ObjectChangedHandler ObjectChanged;
+
+        /// <inheritdoc />
+        object IDynamicValueObject.Value 
+            => Value;
+
+        /// <inheritdoc />
+        public TValue Value {
+            get { return _value; }
+            set {
+                _value = value;
+                ObjectChanged?.Invoke(this, nameof(Value));
+            } 
+        }
+    }
+}
---
+++ source/xcite.csharp/IDynamicValueObject`1.cs
@@ -0,0 +1,8 @@
+﻿namespace xcite.csharp {
+    /// <summary> Generic declaration of <see cref="IDynamicValueObject"/>. </summary>
+    /// <typeparam name="TValue">Type of value</typeparam>
+    public interface IDynamicValueObject<out TValue> : IDynamicValueObject {
+        /// <summary> Observable value that is being referenced. </summary>
+        new TValue Value { get; }
+    }
+}
