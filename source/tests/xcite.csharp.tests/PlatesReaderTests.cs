9d9d7d44e86b0188ee6ebc32bee60e109112c727
Arian Storch <arian.storch@evy-solutions.de>
2018-01-30 17:08:21 +0100

    Introducing test project xcite.csharp

---
+++ source/tests/xcite.csharp.tests/PlatesReaderTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Reflection.Emit;
+using NUnit.Framework;
+
+namespace xcite.csharp.tests {
+    [TestFixture]
+    public class PlatesReaderTests {
+        [Test]
+        public void ReadFromFile() {
+            // Arrange
+            var ctx = TestContext.CurrentContext;
+            string testDir = ctx.TestDirectory;
+            string configFile = Path.Combine(testDir, "TestConfiguration.txt");
+            
+            // Act
+            TestConfiguration cfg = PlatesReader.ReadFromFile<TestConfiguration>(configFile);
+
+            // Assert
+            Assert.IsNotNull(cfg);
+            Assert.AreEqual("Zeichenkettenwert", cfg.PropertyString);
+            Assert.AreEqual(true, cfg.PropertyBool);
+            Assert.AreEqual(65, cfg.PropertyInt);
+        }
+
+    }
+
+    public class TestConfiguration {
+
+        public string PropertyString { get; set; }
+
+        public bool PropertyBool { get; set; }
+
+        public int PropertyInt { get; set; }
+    }
+}
