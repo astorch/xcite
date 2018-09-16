14aa493364149a85fd0cd6ef06da9dc064cca50e
Arian Storch <arian.storch@evy-solutions.de>
2018-09-16 20:27:39 +0200

    Added test project xcite.tdn.tests

---
+++ source/tests/xcite.tdn.tests/TdnIteratorTests.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using NUnit.Framework;
+
+namespace xcite.tdn.tests {
+    [TestFixture]
+    public class TdnIteratorTests {
+        [Test]
+        public void Iterate() {
+            // Arrange
+            string binFolder = TestContext.CurrentContext.TestDirectory;
+            string filePath = Path.Combine(binFolder, "sample1.tdn.txt");
+            string content = File.ReadAllText(filePath);
+
+            // Act
+            using (TdnIterator itr = new TdnIterator(content)) {
+                while (itr.MoveNext()) {
+
+                }
+            }
+
+            // Assert
+            Assert.Inconclusive("Not fully implemented yet");
+        }
+    }
+}
