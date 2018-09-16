4ac54b186360fde22d3c38c60b5459e3524adde6
Arian Storch <arian.storch@evy-solutions.de>
2018-09-16 20:27:09 +0200

    Added new project xcite.tdn

---
+++ source/xcite.tdn/TdnIterator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace xcite.tdn {
+    public class TdnIterator : IDisposable {
+
+        public TdnIterator(string content) {
+
+        }
+
+        public bool MoveNext() {
+            return false;
+        }
+
+        public void Dispose() {
+
+        }
+    }
+}
