76ec5f731d4521f1d1dab01268d84c4354b1368b
Arian Storch <arian.storch@evy-solutions.de>
2019-03-14 17:29:14 +0100

    Added executable test mode

---
+++ source/tests/xcite.clip.tests/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace xcite.clip.tests {
+    public class Program {
+        public static void Main(string[] args) {
+            bool result = Parser.Run(args, OnSuccess, 
+                typeof(UnpackOptions), typeof(CopyOptions));
+        }
+
+        private static void OnSuccess(string arg1, object arg2) {
+            Console.WriteLine();
+        }
+    }
+}
