01bbd81e388b2bbd00cd6c3c3e005aac2923a930
Arian Storch <arian.storch@evy-solutions.de>
2019-07-04 11:33:38 +0200

    Introducing ELockingModel.None

--- source/xcite.logging/ELockingModel.cs
+++ source/xcite.logging/ELockingModel.cs
@@ -1,10 +1,13 @@
 namespace xcite.logging {
     /// <summary> Locking model enumeration </summary>
     public enum ELockingModel {
-        /// <summary> Flag to set that the file is locked from the start of logging to the end. </summary>
-        Exclusive,
-        
+        /// <summary> Flag to set that the file is never lock for other processes (default). </summary>
+        None,
+
         /// <summary> Flag to set that the file is only locked for the minimal amount of time. </summary>
-        Minimal
+        Minimal,
+
+        /// <summary> Flag to set that the file is locked from the start of logging to the end. </summary>
+        Exclusive
     }
 }
