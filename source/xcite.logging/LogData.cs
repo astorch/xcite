c28a4cf052acdda0bed694233026958fa5e4fb39
Arian Storch <arian.storch@evy-solutions.de>
2019-08-03 15:54:41 +0200

    Some documentation

--- source/xcite.logging/LogData.cs
+++ source/xcite.logging/LogData.cs
@@ -3,9 +3,16 @@ using System;
 namespace xcite.logging {
     /// <summary> Describes the data to log. </summary>
     public class LogData {
+        /// <summary> Logger name </summary>
         public string name;
+        
+        /// <summary> Log level </summary>
         public ELogLevel level;
+        
+        /// <summary> Log value </summary>
         public string value;
+        
+        /// <summary> Log excecption </summary>
         public Exception exception;
     }
 }
