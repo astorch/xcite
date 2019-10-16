90b904c65f5ba2a2f7a46fb95dd6ede11dbd1f5c
Arian Storch <arian.storch@evy-solutions.de>
2019-10-16 11:44:22 +0200

    Added locking to avoid concurrency

--- source/xcite.logging/LogStreamManager.cs
+++ source/xcite.logging/LogStreamManager.cs
@@ -1,13 +1,17 @@
 namespace xcite.logging {
     /// <summary> Provides methods to write records to log streams. </summary>
     public class LogStreamManager {
+        private readonly object _accessToken = new object();
+        
         /// <summary>
         /// Writes the given <paramref name="value"/> into each stream of the given <paramref name="logStreams"/>
         /// that match the specified <paramref name="logName"/> and <paramref name="logLevel"/>.
         /// </summary>
         public void Write(ILogStream[] logStreams, string logName, ELogLevel logLevel, string value) {
-            for (int i = -1; ++i != logStreams.Length;)
-                logStreams[i].Write(value);
+            lock (_accessToken) {
+                for (int i = -1; ++i != logStreams.Length;)
+                    logStreams[i].Write(value);
+            }
         }
     }
 }
