393a8a7dc913dd7d8a9068d9b1e74a132e50bd90
Arian Storch <arian.storch@evy-solutions.de>
2019-10-16 10:51:20 +0200

    Added IDisposable to ILogStream

--- source/xcite.logging/ILogStream.cs
+++ source/xcite.logging/ILogStream.cs
@@ -1,6 +1,8 @@
+using System;
+
 namespace xcite.logging {
     /// <summary> Describes a log stream. </summary>
-    public interface ILogStream {
+    public interface ILogStream : IDisposable {
         /// <summary> Writes the specified <paramref name="value"/> into the stream. </summary>
         void Write(string value);
     }
