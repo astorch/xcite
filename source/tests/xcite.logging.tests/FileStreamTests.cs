7fe4d6d89daa5b407e02bfa5aafa67ff7d87b8a2
Arian Storch <arian.storch@evy-solutions.de>
2019-10-16 11:49:15 +0200

    Added test for daily rolling

--- source/tests/xcite.logging.tests/FileStreamTests.cs
+++ source/tests/xcite.logging.tests/FileStreamTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using NUnit.Framework;
 using FileStream = xcite.logging.streams.FileStream;
@@ -19,6 +20,46 @@ namespace xcite.logging.tests {
             // Assert
             string fileText = File.ReadAllText(logFilePath);
             Assert.AreEqual(logText, fileText);
+        }
+
+        [Test]
+        public void WriteDailyRolling() {
+            // Arrange
+            string[] files = Directory.GetFiles(TestContext.CurrentContext.TestDirectory, "*.txt");
+            for (int i = -1; ++i != files.Length;)
+                File.Delete(files[i]);
+            
+            string logFilePath = Path.Combine(TestContext.CurrentContext.TestDirectory, "_log.txt");
+            string logTextYesterday = "INFO 30.12.2018 19:44:51 FileStreamTests - Say hi from yesterday.\r\n";
+            string logTextToday = "INFO 31.12.2018 19:44:51 FileStreamTests - Say hi from today.\r\n";
+            FileStream fileStream = new FileStream {FileName = logFilePath, DailyRolling = true};
+            DateTime today = DateTime.Today;
+            DateTime yesterday = today.AddDays(-1);
+
+            // Act
+            fileStream.Write(logTextYesterday);
+            ((FileStream._Bender) fileStream).SetLastWrite(yesterday);
+            fileStream.Write(logTextToday);
+            
+            fileStream.Dispose();
+
+            // Assert
+            string year = yesterday.Year.ToString();
+            string month = yesterday.Month.ToString("00");
+            string day = yesterday.Day.ToString("00");
+            string logFileYesterdayName = $"_log-{year}-{month}-{day}.txt";
+
+            string logFileYesterday = Path.Combine(TestContext.CurrentContext.TestDirectory, logFileYesterdayName);
+            string logFileToday = Path.Combine(TestContext.CurrentContext.TestDirectory, "_log.txt");
+            
+            Assert.IsTrue(File.Exists(logFileYesterday));
+            Assert.IsTrue(File.Exists(logFileToday));
+            
+            string todayFileText = File.ReadAllText(logFileToday);
+            Assert.AreEqual(logTextToday, todayFileText);
+            
+            string yesterdayFileText = File.ReadAllText(logFileYesterday);
+            Assert.AreEqual(logTextYesterday, yesterdayFileText);
         }
     }
 }
