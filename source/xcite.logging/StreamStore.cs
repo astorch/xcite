4e7c87cce15b3adffc95a6b6f62435c04c02b5a1
Philippe Barias <philippe.barias@evy-solutions.de>
2020-10-09 16:17:35 +0200

    Implemented loading of external ILogStream implementations

--- source/xcite.logging/StreamStore.cs
+++ source/xcite.logging/StreamStore.cs
@@ -10,22 +10,57 @@ namespace xcite.logging {
         };
         
         /// <summary>
+        /// Loads the <see cref="ILogStream"/> instance specified by <paramref name="name"/>.
+        /// Returns <c>null</c> if the name neither matches a predefined nor a custom implementation (external assembly).
+        /// </summary>
+        public static ILogStream GetStreamInstance(string name) {
+            ILogStream stream =  GetWellKnownStreamInstance(name);
+            return stream ?? GetStreamInstanceFromAssembly(name);
+        }
+
+        /// <summary>
         /// Returns a new instance of <see cref="ILogStream"/> that owns
         /// the specified <paramref name="shortName"/>. 
         /// </summary>
-        public static ILogStream GetStreamInstance(string shortName) {
+        private static ILogStream GetWellKnownStreamInstance(string shortName) {
             if (string.IsNullOrEmpty(shortName)) return null;
-            string nmlc = shortName.ToLower();
+            string nameLowerCase = shortName.ToLower();
             
             for (int i = -1; ++i != StreamTypes.Length;) {
                 Type streamType = StreamTypes[i];
-                string fnlc = streamType.FullName?.ToLower();
-                if (fnlc == null) continue;
+                string fullNameLowerCase = streamType.FullName?.ToLower();
+                if (fullNameLowerCase == null) continue;
 
-                if (fnlc.EndsWith(nmlc)) return (ILogStream) Activator.CreateInstance(streamType);
+                if (fullNameLowerCase.EndsWith(nameLowerCase)) return (ILogStream) Activator.CreateInstance(streamType);
             }
 
             return null;
+        }
+
+        /// <summary>
+        /// Returns a new instance based on the <paramref name="assemblyQualifiedName"/>,
+        /// if it is a subclass of <see cref="ILogStream"/>. Returns <c>null</c> otherwise.
+        /// </summary>
+        private static ILogStream GetStreamInstanceFromAssembly(string assemblyQualifiedName) {
+            bool successful = TryGetType(assemblyQualifiedName, out Type type);
+            if (!successful) return null;
+            
+            if (!typeof(ILogStream).IsAssignableFrom(type)) return null;
+            return (ILogStream) Activator.CreateInstance(type);
+        }
+
+        /// <summary>
+        /// Tries to load the type specified by <paramref name="typeName"/>.
+        /// Returns <c>true</c> if successful and <c>false</c> otherwise.
+        /// </summary>
+        private static bool TryGetType(string typeName, out Type type) {
+            type = null;
+            try {
+                type = Type.GetType(typeName);
+                return type != null;
+            } catch {
+                return false;
+            }
         }
     }
 }
